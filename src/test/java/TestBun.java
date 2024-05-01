3b7fffd4c073f8ba2092326a7d8f0e029e909825
igavrilin <igavrilin@rbc.ru>
2024-05-01 19:35:05 +0300

    Diplom-1

---
+++ src/test/java/TestBun.java
@@ -0,0 +1,18 @@
+import org.junit.Assert;
+import org.junit.Test;
+import org.mockito.Mockito;
+import org.mockito.Spy;
+import praktikum.Bun;
+
+public class TestBun {
+    Bun bun = new Bun("Булка", 3.14f);
+
+    @Test
+    public void testName(){
+        Assert.assertEquals("Булка", bun.getName());
+    }
+    @Test
+    public void testPrice(){
+        Assert.assertTrue(3.14f == bun.getPrice());
+    }
+}
